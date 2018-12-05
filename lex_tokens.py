494f8b71c6fe9fc48909119096fc57a6a812338e
Nikita Volkov <39171924+mutedSpectre@users.noreply.github.com>
2018-12-05 22:38:14 +0300

    Add lexer.

---
+++ lex_tokens.py
@@ -0,0 +1,15 @@
+import lexer
+
+RESERVED = 'RESERVED'
+INT      = 'INT'
+ID       = 'ID'
+
+tokens_list = [
+    (r'\=', RESERVED),
+    (r'\+', RESERVED),
+    (r'\-', RESERVED),
+    (r'[0-9]',   INT),
+]
+
+def token_lex(characters):
+    return lexer.lex(characters, tokens_list)
---
+++ lexer.py
@@ -0,0 +1,25 @@
+import sys
+import re
+
+
+def lex(characters, tokens_list):
+    pos = 0
+    tokens = []
+    while pos < len(characters):
+        match = None
+        for token_expr in tokens_list:
+            pattern, tag = token_expr
+            regex = re.compile(pattern)
+            match = regex.match(characters, pos)
+            if match:
+                text = match.group(0)
+                if tag:
+                    token = (text, tag)
+                    tokens.append(token)
+                break
+        if not match:
+            sys.stderr.write('Illegal character: %s\n' % characters[pos])
+            sys.exit(1)
+        else:
+            pos = match.end(0)
+    return tokens
