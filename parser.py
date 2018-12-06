d1127606cf19c7e7f5a5cb59bbed215aa8796721
Nikita Volkov <39171924+mutedSpectre@users.noreply.github.com>
2018-12-07 01:29:12 +0300

    Create parser.py

---
+++ parser.py
@@ -0,0 +1,84 @@
+class Result:
+    def __init__(self, value, pos):
+        self.value = value
+        self.pos = pos
+
+    def __repr__(self):
+        return 'Result(%s, %d)' % (self.value, self.pos)
+
+class Parser:
+    def __call__(self, tokens, pos):
+        return None
+
+    def __add__(self, other):
+        return Concat(self, other)
+
+    def __mul__(self, other):
+        return Exp(self, other)
+
+    def __or__(self, other):
+        return Alternate(self, other)
+
+    def __xor__(self, function):
+        return Process(self, function)
+
+class Reserved(Parser):
+    def __init__(self, value, tag):
+        self.value = value
+        self.tag = tag
+
+    def __call__(self, tokens, pos):
+        if pos < len(tokens) and \
+            tokens[pos][0] == self.value and \
+            tokens[pos][1] is self.tag:
+                return Result(tokens[pos][0], pos + 1)
+        else:
+            return None
+
+class Tag(Parser):
+    def __init__(self, tag):
+        self.tag = tag
+
+    def __call__(self, tokens, pos):
+        if pos < len(tokens) and tokens[pos][1] is self.tag:
+            return Result(tokens[pos][0], pos + 1)
+        else:
+            return None
+
+class Concat(Parser):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+    def __call__(self, tokens, pos):
+        left_result = self.left(tokens, pos)
+        if left_result:
+            right_result = self.right(tokens, left_result.pos)
+            if right_result:
+                combined_value = (left_result.value, right_result.value)
+                return Result(combined_value, right_result.pos)
+        return None
+
+class Alternate(Parser):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+    def __call__(self, tokens, pos):
+        left_result = self.left(tokens, pos)
+        if left_result:
+            return left_result
+        else:
+            right_result = self.right(tokens, pos)
+            return right_result
+
+class Opt(Parser):
+    def __init__(self, parser):
+        self.parser = parser
+
+    def __call__(self, tokens, pos):
+        result = self.parser(tokens, pos)
+        if result:
+            return result
+        else:
+            return Result(None, pos)
