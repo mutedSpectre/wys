07b93c0739db5265760c447d678567d295ec4a7b
Nikita Volkov <39171924+mutedSpectre@users.noreply.github.com>
2018-12-10 21:46:38 +0300

    Update ast.py

--- ast.py
+++ ast.py
@@ -10,12 +10,21 @@ class IntAexp(Aexp):
     def __repr__(self):
         return 'IntAexp(%d)' % self.i
 
+    def eval(self, env):
+        return self.i
+
 class VarAexp(Aexp):
     def __init__(self, name):
         self.name = name
 
     def __repr__(self):
         return 'VarAexp(%s)' % self.name
+
+    def eval(self, env):
+        if self.name in env:
+            return env[self.name]
+        else:
+            return 0
 
 class BinopAexp(Aexp):
     def __init__(self, op, left, right):
@@ -26,16 +35,64 @@ class BinopAexp(Aexp):
     def __repr__(self):
         return 'BinopAexp(%s, %s, %s)' % (self.op, self.left, self.right)
 
+    def eval(self, env):
+        left_value = self.left.eval(env)
+        right_value = self.right.eval(env)
+        if self.op == '+':
+            value = left_value + right_value
+        elif self.op == '-':
+            value = left_value - right_value
+        elif self.op == '*':
+            value = left_value * right_value
+        elif self.op == '/':
+            value = left_value / right_value
+        else:
+            raise RuntimeError('unknown operator: ' + self.op)
+        return value
+
 class Bexp(Equality):
     pass
 
 class RelopBexp(Bexp):
     def __init__(self, op, left, right):
-        ...
+        self.op = op
+        self.left = left
+        self.right = right
+
+    def __repr__(self):
+        return 'RelopBexp(%s, %s, %s)' % (self.op, self.left, self.right)
+
+    def eval(self, env):
+        left_value = self.left.eval(env)
+        right_value = self.right.eval(env)
+        if self.op == '<':
+            value = left_value < right_value
+        elif self.op == '<=':
+            value = left_value <= right_value
+        elif self.op == '>':
+            value = left_value > right_value
+        elif self.op == '>=':
+            value = left_value >= right_value
+        elif self.op == '=':
+            value = left_value == right_value
+        elif self.op == '!=':
+            value = left_value != right_value
+        else:
+            raise RuntimeError('unknown operator: ' + self.op)
+        return value
 
 class AndBexp(Bexp):
     def __init__(self, left, right):
-        ...
+        self.left = left
+        self.right = right
+
+    def __repr__(self):
+        return 'AndBexp(%s, %s)' % (self.left, self.right)
+
+    def eval(self, env):
+        left_value = self.left.eval(env)
+        right_value = self.right.eval(env)
+        return left_value and right_value
 
 class OrBexp(Bexp):
     def __init__(self, left, right):
