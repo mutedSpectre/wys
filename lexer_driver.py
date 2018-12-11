8ee64df74b75f883e27f143762cbd4375af2bfac
Nikita Volkov <39171924+mutedSpectre@users.noreply.github.com>
2018-12-11 20:09:22 +0300

    Create lexer_driver.py

---
+++ lexer_driver.py
@@ -0,0 +1,11 @@
+import sys
+from lex_tokens import *
+
+if __name__ == '__main__':
+    filename = sys.argv[1]
+    file = open(filename)
+    characters = file.read()
+    file.close()
+    tokens = imp_lex(characters)
+    for token in tokens:
+        print token
