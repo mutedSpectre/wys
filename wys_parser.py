b9bc00e72b664155d034366bfd4a0645f0732c11
Nikita Volkov <39171924+mutedSpectre@users.noreply.github.com>
2018-12-11 21:49:33 +0300

    Update wys_parser.py

--- wys_parser.py
+++ wys_parser.py
@@ -3,103 +3,45 @@ from combinators import *
 from wys_ast import *
 from functools import reduce
 
-#Basic parser
+
+# Basic parsers
 def keyword(kw):
     return Reserved(kw, RESERVED)
 
-id = Tag(ID)
-num = Tag(INT) ^ (lambda i: int(i))
 
+num = Tag(INT) ^ (lambda i: int(i))
+id = Tag(ID)
+
+
+# Top level parser
 def wys_parse(tokens):
     ast = parser()(tokens, 0)
     return ast
 
-def aexp_value():
-    return (num ^ (lambda i: IntAexp(i))) | \
-           (id  ^ (lambda v: VarAexp(v)))
 
-def process_group(parsed):
-    ((_, p), _) = parsed
-    return p
+def parser():
+    return Phrase(stmt_list())
 
-def aexp_group():
-    return keyword('(') + Lazy(aexp) + keyword(')') ^ process_group
 
-def aexp_term():
-    return aexp_value() | aexp_group()
+# Statements
+def stmt_list():
+    separator = keyword(';') ^ (lambda x: lambda l, r: CompoundStatement(l, r))
+    return Exp(stmt(), separator)
 
-def process_binop(op):
-    return lambda l, r: BinopAexp(op, 1, r)
 
-def any_operator_in_list(ops):
-    op_parsers = [keyword(op) for op in ops]
-    parser = reduce(lambda l, r: l | r, op_parsers)
-    return parser
+def stmt():
+    return assign_stmt() | \
+           if_stmt() | \
+           while_stmt()
 
-aexp_precedence_levels = [
-    ['*', '/'],
-    ['+', '-'],
-]
-
-def precedence(value_parser, precedence_levels, combine):
-    def op_parser(precedence_level):
-        return any_operator_in_list(precedence_level) ^ combine
-    parser = value_parser * op_parser(precedence_levels[0])
-    for precedence_level in precedence_levels[1:]:
-        parser = parser * op_parser(precedence_level)
-    return parser
-
-def aexp():
-    return precedence(aexp_term(),
-                      aexp_precedence_levels,
-                      process_binop)
-
-def process_relop(parsed):
-    ((left, op), right) = parsed
-    return RelopBexp(op, left, right)
-
-def bexp_relop():
-    relops = ['<', '<=', '>', '>=', '=', '!=']
-    return aexp() + any_operator_in_list(relops) + aexp() ^ process_relop
-
-def bexp_not():
-    return keyword('not') + Lazy(bexp_term) ^ (lambda parsed: NotBexp(parsed[1]))
-
-def bexp_group():
-    return keyword('(') + Lazy(bexp) + keyword(')') ^ process_group
-
-def bexp_term():
-    return bexp_not()    | \
-           bexp_relop()  | \
-           bexp_group()
-
-bexp_precedence_levels = [
-    ['and'],
-    ['or'],
-]
-
-def process_logic(op):
-    if op == 'and':
-        return lambda l, r: AndBexp(1, r)
-    elif op == 'or':
-        return lambda l, r: OrBexp(1, r)
-    else:
-        raise RuntimeError('unknown logic operator: ', + op)
-
-def bexp():
-    return precedence(bexp_term(),
-                      bexp_precedence_levels,
-                      process_logic)
 
 def assign_stmt():
     def process(parsed):
         ((name, _), exp) = parsed
         return AssignStatement(name, exp)
+
     return id + keyword(':=') + aexp() ^ process
 
-def stmt_list():
-    separator = keyword(';') ^ (lambda x: lambda l, r: CompoundStatement(l, r))
-    return Exp(stmt(), separator)
 
 def if_stmt():
     def process(parsed):
@@ -109,23 +51,117 @@ def if_stmt():
         else:
             false_stmt = None
         return IfStatement(condition, true_stmt, false_stmt)
+
     return keyword('if') + bexp() + \
            keyword('then') + Lazy(stmt_list) + \
            Opt(keyword('else') + Lazy(stmt_list)) + \
            keyword('end') ^ process
 
+
 def while_stmt():
     def process(parsed):
         ((((_, condition), _), body), _) = parsed
         return WhileStatement(condition, body)
+
     return keyword('while') + bexp() + \
            keyword('do') + Lazy(stmt_list) + \
            keyword('end') ^ process
 
-def stmt():
-    return assign_stmt() | \
-           if_stmt()     | \
-           while_stmt()
 
-def parser():
-    return Phrase(stmt_list())
+# Boolean expressions
+def bexp():
+    return precedence(bexp_term(),
+                      bexp_precedence_levels,
+                      process_logic)
+
+
+def bexp_term():
+    return bexp_not() | \
+           bexp_relop() | \
+           bexp_group()
+
+
+def bexp_not():
+    return keyword('not') + Lazy(bexp_term) ^ (lambda parsed: NotBexp(parsed[1]))
+
+
+def bexp_relop():
+    relops = ['<', '<=', '>', '>=', '=', '!=']
+    return aexp() + any_operator_in_list(relops) + aexp() ^ process_relop
+
+
+def bexp_group():
+    return keyword('(') + Lazy(bexp) + keyword(')') ^ process_group
+
+
+# Arithmetic expressions
+def aexp():
+    return precedence(aexp_term(),
+                      aexp_precedence_levels,
+                      process_binop)
+
+
+def aexp_term():
+    return aexp_value() | aexp_group()
+
+
+def aexp_group():
+    return keyword('(') + Lazy(aexp) + keyword(')') ^ process_group
+
+
+def aexp_value():
+    return (num ^ (lambda i: IntAexp(i))) | \
+           (id ^ (lambda v: VarAexp(v)))
+
+
+# An IMP-specific combinator for binary operator expressions (aexp and bexp)
+def precedence(value_parser, precedence_levels, combine):
+    def op_parser(precedence_level):
+        return any_operator_in_list(precedence_level) ^ combine
+
+    parser = value_parser * op_parser(precedence_levels[0])
+    for precedence_level in precedence_levels[1:]:
+        parser = parser * op_parser(precedence_level)
+    return parser
+
+
+# Miscellaneous functions for binary and relational operators
+def process_binop(op):
+    return lambda l, r: BinopAexp(op, l, r)
+
+
+def process_relop(parsed):
+    ((left, op), right) = parsed
+    return RelopBexp(op, left, right)
+
+
+def process_logic(op):
+    if op == 'and':
+        return lambda l, r: AndBexp(l, r)
+    elif op == 'or':
+        return lambda l, r: OrBexp(l, r)
+    else:
+        raise RuntimeError('unknown logic operator: ' + op)
+
+
+def process_group(parsed):
+    ((_, p), _) = parsed
+    return p
+
+
+def any_operator_in_list(ops):
+    op_parsers = [keyword(op) for op in ops]
+    parser = reduce(lambda l, r: l | r, op_parsers)
+    return parser
+
+
+# Operator keywords and precedence levels
+aexp_precedence_levels = [
+    ['*', '/'],
+    ['+', '-'],
+]
+
+bexp_precedence_levels = [
+    ['and'],
+    ['or'],
+]
