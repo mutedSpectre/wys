82be1190b0a87ca6225896936b73f2c1185f0df5
Nikita Volkov <39171924+mutedSpectre@users.noreply.github.com>
2018-12-13 19:02:35 +0300

    Extension check added

--- wys.py
+++ wys.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import sys
+import os
 from wys_parser import *
 from lex_tokens import *
 
@@ -11,13 +12,20 @@ def usage():
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         usage()
+
+    filename, file_extension = os.path.splitext(sys.argv[1])   #Extension check
+    if file_extension != '.wys':
+        usage()
+
     filename = sys.argv[1]
     text = open(filename).read()
     tokens = token_lex(text)
     parse_result = wys_parse(tokens)
+
     if not parse_result:
         sys.stderr.write('Parse error!\n')
         sys.exit(1)
+
     ast = parse_result.value
     env = {}
     ast.eval(env)
