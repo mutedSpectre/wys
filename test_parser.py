c93e76a9fbc79d654a64524a6470a58e87893217
Nikita Volkov <39171924+mutedSpectre@users.noreply.github.com>
2018-12-20 22:53:06 +0300

    Add test for parser function 'print'

--- test_parser.py
+++ test_parser.py
@@ -7,8 +7,11 @@ class ParserStatementTests(unittest.TestCase):
     def parser_test(self, code, parser, expected):
         tokens = lex_tokens.token_lex(code)
         result = parser(tokens, 0)
-        self.assertNotEquals(None, result)
-        self.assertEquals(expected, result.value)
+        self.assertNotEqual(None, result)
+        self.assertEqual(expected, result.value)
 
     def test_assign_stmt(self):
         self.parser_test('x = 1', wys_parser.stmt_list(), wys_ast.AssignStatement('x', wys_parser.IntAexp(1)))
+
+    def test_print_stmt(self):
+        self.parser_test('print x', wys_parser.stmt_list(), wys_ast.PrintStatement('x'))
