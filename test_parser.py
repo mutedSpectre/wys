270e5ec1079988e46f16630edadf43305f10d729
Nikita Volkov <39171924+mutedSpectre@users.noreply.github.com>
2018-12-20 22:49:11 +0300

    Add test for parser function 'assign'

--- test_parser.py
+++ test_parser.py
@@ -1,6 +1,7 @@
 import unittest
 import wys_parser
 import lex_tokens
+import wys_ast
 
 class ParserStatementTests(unittest.TestCase):
     def parser_test(self, code, parser, expected):
@@ -8,3 +9,6 @@ class ParserStatementTests(unittest.TestCase):
         result = parser(tokens, 0)
         self.assertNotEquals(None, result)
         self.assertEquals(expected, result.value)
+
+    def test_assign_stmt(self):
+        self.parser_test('x = 1', wys_parser.stmt_list(), wys_ast.AssignStatement('x', wys_parser.IntAexp(1)))
