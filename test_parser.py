1528ea53486b5a81a17433f37cd90b1fdf78c6e4
Nikita Volkov <39171924+mutedSpectre@users.noreply.github.com>
2018-12-31 22:03:01 +0300

    Update test for if statement

--- test_parser.py
+++ test_parser.py
@@ -19,10 +19,10 @@ class ParserStatementTests(unittest.TestCase):
 
     def test_input_stmt(self):
         self.parser_test('input x', wys_parser.stmt_list(), wys_ast.InputStatement('x'))
-    
+
     def test_if_stmt(self):
-        code = 'if 1 < 2 then x := 3 else x := 4 end'
+        code = 'if 1 < 2 then x = 3 else x = 4 end'
         expected = wys_ast.IfStatement(wys_ast.RelopBexp('<', wys_ast.IntAexp(1), wys_ast.IntAexp(2)),
-                               wys_ast.AssignStatement('x', wys_ast.IntAexp(3)),
-                               wys_ast.AssignStatement('x', wys_ast.IntAexp(4)))
+                                       wys_ast.AssignStatement('x', wys_ast.IntAexp(3)),
+                                       wys_ast.AssignStatement('x', wys_ast.IntAexp(4)))
         self.parser_test(code, wys_parser.stmt_list(), expected)
